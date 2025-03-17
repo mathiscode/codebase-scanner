d546a137c57c7660dd7bb4c18d4995fce3b99942
mathiscode <code@mathis.network>
2025-03-16 21:06:53 -0500

    cleanup docker output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ echo "- https://github.com/mathiscode/codebase-scanner"
 echo "Enter the repo url you want to scan: "
 read repo_url
 git clone --quiet $repo_url /opt/repo
-npm_config_yes=true npx @mathiscode/codebase-scanner /opt/repo
+npm config set update-notifier false
+NODE_NO_WARNINGS=true npm_config_yes=true npx @mathiscode/codebase-scanner /opt/repo
